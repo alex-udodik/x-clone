7ca93194e2439b38b9e6d55d67678793e2a12a85
alex-udodik <alex.udodik@wsu.edu>
2026-02-25 23:00:13 -0800

    changed homepage. Fixed naming for Tweet.module.scss

--- x-clone/src/app/page.tsx
+++ x-clone/src/app/page.tsx
@@ -1,13 +1,11 @@
+import HomePage from "../../components/HomePage";
 import styles from "./page.module.scss";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Home page</h1>
-        <Link href='/signup'>Sign up</Link>
-        <Link href='/login'>Log in</Link>
+        <HomePage />
       </main>
     </div>
   );
